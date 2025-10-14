7ca1ef96c2c32ce26fc8bb67558b6e2eab143a91
EstbnV <elestebancva@gmail.com>
2025-10-14 15:18:05 -0600

    regresar configuracion del puppeter

--- server.js
+++ server.js
@@ -27,11 +27,7 @@ app.use((req, res, next) => {
 // Así nunca tendrás que escanear el QR de nuevo
 const client = new Client({
   authStrategy: new LocalAuth(),
-  puppeteer: { //Esto lo utilizo para un VPS, si no lo utilizas puedes eliminar esta sección
-    headless: true,
-    executablePath: '/usr/bin/chromium',  
-    args: ['--no-sandbox', '--disable-setuid-sandbox']
-  }
+  
 });
 
 // Generar QR si no hay sesión
